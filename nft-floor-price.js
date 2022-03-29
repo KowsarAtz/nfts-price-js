30eeb34e34db618751c008d7dc9c32669de7bc34
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-30 02:52:56 +0430

    Enhance fetching floor price query

--- nft-floor-price.js
+++ nft-floor-price.js
@@ -1,94 +1,6 @@
 const bigDecimal = require("js-big-decimal");
 const { executeQuery } = require("./graphql.js");
-const { ETH_TO_WEI, DIVISION_PRECISION, PAGE_SIZE } = require("./constants.js");
-
-const getTokenFloorPrice = async (
-    collection,
-    tokenId,
-    fromTimestamp,
-    toTimestamp
-) => {
-    const timeFilter =
-        fromTimestamp == null || toTimestamp == null
-            ? ""
-            : `, where: {timestamp_gte: ${fromTimestamp}, timestamp_lte: ${toTimestamp}}`;
-
-    const query = `{
-        token(id: "${collection}:${tokenId}"){
-            sales(first: 1, orderBy: price, orderDirection: asc${timeFilter}){
-                price
-            }
-        }
-    }`;
-
-    const sales = (await executeQuery(query))?.data?.token?.sales;
-
-    if (sales == null) throw new Error("token not found");
-
-    return sales.length > 0
-        ? new bigDecimal(sales[0].price)
-              .divide(ETH_TO_WEI, DIVISION_PRECISION)
-              .getValue()
-        : null;
-};
-
-const getCollectionPartialTokensFloorPrices = async (
-    collection,
-    fromTimestamp,
-    toTimestamp,
-    skip = 0
-) => {
-    const timeFilter =
-        fromTimestamp == null || toTimestamp == null
-            ? ""
-            : `, where: {timestamp_gte: ${fromTimestamp}, timestamp_lte: ${toTimestamp}}`;
-
-    const query = `{
-        collection(id: "${collection}"){
-            tokens(skip: ${skip}, first: ${PAGE_SIZE}, orderBy: tokenId, orderDirection: asc){
-                sales(first: 1, orderBy: price, orderDirection: asc${timeFilter}){
-                    price
-                }
-            }
-        }
-    }`;
-
-    const tokens = (await executeQuery(query))?.data?.collection?.tokens;
-
-    if (tokens == null) throw new Error("collection not found");
-
-    return tokens.map((token) =>
-        token.sales != null && token.sales.length > 0
-            ? new bigDecimal(token.sales[0].price)
-            : null
-    );
-};
-
-const getCollectionFloorPrice = async (
-    collection,
-    fromTimestamp = null,
-    toTimestamp = null
-) => {
-    let floorPrices = [];
-    let latestFetchResultCount = PAGE_SIZE;
-    while (latestFetchResultCount == PAGE_SIZE) {
-        const result = await getCollectionPartialTokensFloorPrices(
-            collection,
-            fromTimestamp,
-            toTimestamp,
-            floorPrices.length
-        );
-        floorPrices.push(...result);
-        latestFetchResultCount = result.length;
-    }
-    floorPrices = floorPrices.filter((price) => price != null);
-    return floorPrices.length > 0
-        ? floorPrices
-              .reduce((a, b) => (a.compareTo(b) < 0 ? a : b))
-              .divide(ETH_TO_WEI, DIVISION_PRECISION)
-              .getValue()
-        : null;
-};
+const { ETH_TO_WEI, DIVISION_PRECISION } = require("./constants.js");
 
 const getFloorPrice = async (
     collection,
@@ -96,18 +8,32 @@ const getFloorPrice = async (
     toTimestamp = null,
     tokenId = null
 ) => {
-    if (tokenId == null)
-        return await getCollectionFloorPrice(
-            collection,
-            fromTimestamp,
-            toTimestamp
+    const timeFilter =
+        fromTimestamp == null || toTimestamp == null
+            ? ""
+            : `, timestamp_gte: ${fromTimestamp}, timestamp_lte: ${toTimestamp}`;
+
+    const tokenFilter =
+        tokenId == null
+            ? `token_starts_with: "${collection.toLowerCase()}:"`
+            : `token: "${collection.toLowerCase()}:${tokenId}"`;
+
+    const query = `{
+        sales(first: 1, orderBy: price, orderDirection: asc, where : {${tokenFilter}${timeFilter}}){
+            price
+        }
+    }`;
+
+    const sales = (await executeQuery(query))?.data?.sales;
+
+    if (sales == null || sales.length == 0)
+        throw new Error(
+            `${tokenId == null ? "collection" : "token"} not found`
         );
-    return await getTokenFloorPrice(
-        collection,
-        tokenId,
-        fromTimestamp,
-        toTimestamp
-    );
+
+    return new bigDecimal(sales[0].price)
+        .divide(ETH_TO_WEI, DIVISION_PRECISION)
+        .getValue();
 };
 
 module.exports = { getFloorPrice };
